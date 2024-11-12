f81c248956c182759a5998740a82157ceb3ef5ee
Tomas Szabo <bady.tomas33@gmail.com>
2024-11-12 16:45:44 +0000

    Enemy ScriptableObject, Simple Enemy AI
    Implemented scriptable object to accommodate different enemy types with different attributes. Added NavMesh using new navigation AI package and simple enemy follow.

---
+++ Darkspire/Assets/GD/My Assets/Scripts/ScriptableObjects/EnemyAttributes.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+
+// Scriptable object that holds values for different enemies
+// Learned how to implement from: https://docs.unity3d.com/Manual/class-ScriptableObject.html
+
+[CreateAssetMenu(fileName = "NewEnemyStats", menuName = "Enemy/Enemy Attributes")]
+public class EnemyAttributes : ScriptableObject
+{
+    public string enemyName;
+    public float health;
+    public float attackDamage;
+    public float attackRange;
+    public float movementSpeed;
+    public float detectionRange;
+}
