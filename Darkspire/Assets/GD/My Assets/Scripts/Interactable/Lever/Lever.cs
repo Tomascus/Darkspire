b53ebfd31d84ba4ba49731d0ad6dc84f69da050e
Smartisek <domalipdominik@gmail.com>
2024-11-21 19:31:13 +0000

    ⭐ ⭐ Interacting with doors, gate, finding key
    Made a few changes so that the player now can open door only when he finds a key, then door opens, also gates now react to lever pull and opens. Added the NPC with dialogue into the map

---
+++ Darkspire/Assets/GD/My Assets/Scripts/Interactable/Lever/Lever.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    [SerializeField] private Gate gate; // Serialize the field to assign in the Inspector
+    private bool playerInRange = false;
+
+    private void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            PressLever();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    private void PressLever()
+    {
+        if (gate != null)
+        {
+            gate.StartMoving();
+        }
+    }
+}
