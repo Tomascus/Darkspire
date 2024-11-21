6dfb987fdc797a4272b5d6c18e8916255eab9c53
DovydasJakuciunas <d00244777@student.dkit.ie>
2024-11-21 16:22:37 +0000

    ✨ Small Little Fixes
    Adding same prefab to all scenes

--- Darkspire/Assets/GD/My Assets/Scripts/Audio/Play(Sounds)/PlayNPCTalk.cs
+++ Darkspire/Assets/GD/My Assets/Scripts/Audio/Play(Sounds)/PlayNPCTalk.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class PlayNPCTalk : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayIntroMerchant()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SoundManager.PlaySound(SoundType.TALK);
     }
 }
