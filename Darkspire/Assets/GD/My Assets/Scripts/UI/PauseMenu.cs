97e9ba03e63be6967d033e681bdf9dfd666d778f
DovydasJakuciunas <d00244777@student.dkit.ie>
2024-11-22 11:31:58 +0000

    🐛 Attempting to Add Pause Menu

---
+++ Darkspire/Assets/GD/My Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused;   
+
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+
+}
