2f4fb5d112eae3c3346541a1fc67bc2c45962c6f
Tomas Szabo <bady.tomas33@gmail.com>
2024-12-14 22:58:44 +0000

    ✨ Boss NPC, Intro fixes
    Added an npc that spawns after killing the boss, which is a part of our game ending. Fixed the intro scene so it works properly and loads when going from main menu

--- Darkspire/Assets/GD/My Assets/Scripts/Settings/MainMenu.cs
+++ Darkspire/Assets/GD/My Assets/Scripts/Settings/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     //Once play button is clicked, load the MapPrototype scene
     public void PlayGame()
     {
-        SceneManager.LoadScene("MapPrototype");
+        SceneManager.LoadScene("Intro");
     }
     //Once quit button is clicked, quit the game
     public void QuitGame()
